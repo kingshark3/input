e5e36654683657fb36ab96f0aa3d32d5d5e0fc9f
Jayden Little <littljay000@warren.k12.in.us>
2024-08-28 09:56:37 -0400

    intial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+name = input("What is your name: ")
+droids = input("How many droid do you want to meet? ")
+wookies = input("How many wookies do you want to meet")
+print(name, "wants to meet", droids, "droids and ", wookies, "Wookiees!")
